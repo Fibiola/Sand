25dd00e25e7df2b6f37ea7f007aad541199e0efd
Fibiola <natasha.pristovsek@gmail.com>
2013-08-16 02:04:32 +0200

    few changes

--- PythonFun/Crawler/Crawler.py
+++ PythonFun/Crawler/Crawler.py
@@ -7,8 +7,8 @@
 # start_link to be the value of the position
 # where the first '<a href=' occurs in a page.
 
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
+page ="""<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com"> <div> <a href="http://www.newlink.com">"""
 
 start_link = page.find("<a href=", 0)
 
@@ -19,7 +19,31 @@ start_link = page.find("<a href=", 0)
 
 # page = contents of a web page
 
-start_url = page.find('http', start_link)
-end_url = page.find('"', start_url)
-url = page[start_url:end_url]
-print url
+#start_url = page.find('"', start_link)
+#end_url = page.find('"', start_url + 1)
+#url = page[start_url + 1 :end_url]
+#print url
+
+#page = page[end_url:]
+#start_link = page.find('<a href=')
+#start_url = page.find('"', start_link)
+#end_url = page.find('"', start_url + 1)
+#url = page[start_url + 1:end_url]
+#print url 
+
+# You can repeat this manually for all the links on the page. Or we use functions.
+
+# PROCEDURE - AKA FUNCTION 
+
+# defined function - we change the name to a more generic name s as page. 
+def get_next_target(s):
+	start_link = s.find('<a href=')
+	start_quote = s.find('"', start_link)
+	end_quote = s.find('"', start_quote + 1)
+	url = s[start_quote + 1:end_quote]
+	return url, end_quote
+
+print get_next_target(page)
+#side effect : something we see -> but we don't get it as an output.
+#find is also a procedure! 
+
--- PythonFun/problemset1.py
+++ PythonFun/problemset1.py
@@ -52,3 +52,58 @@ new_value = x + 0.5
 string = str(new_value)
 decimal = string.find(".")
 print string[:decimal]
+
+#UNIT 2
+
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
+
+def find_second(search,target):
+	first = search.find(target)
+	return search.find(target, first + 1)
+    	
+
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
+
+
+# UNIT 2 - BIGGEST
+# Define a procedure, biggest, that takes three
+# numbers as inputs and returns the largest of
+# those three numbers.
+
+def bigger(a,b):
+	if a > b:
+		return a
+	else:
+		return b
+
+def biggest(a,b,c):
+	return bigger((a, b), c)
+
+print max(a, b, c)
+
+print biggest(3, 6, 9)
+#>>> 9
+
+print biggest(6, 9, 3)
+#>>> 9
+
+print biggest(9, 3, 6)
+#>>> 9
+
+#print biggest(3, 3, 9)
+#>>> 9
+
+#print biggest(9, 3, 9)
+#>>> 9
