3696ed4a534ea8c8a858cc74260026c8ed1f7f9f
Fibiola <natasha.pristovsek@gmail.com>
2013-09-15 01:32:58 +0200

    jQuery slider and whiteboard

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,13 +1,19 @@
 //this only affects the div we want it to affect - instead of all divs simultaniously.
 $(document).ready(function() {
-    $('div').mouseenter(function() {
+    $('.buttons').mouseenter(function() {
         $(this).fadeTo('fast', 0);
-});
+    });
 });
 
 
-$(document).read(function() {
-    $('p').mouseenter(function(){
-        $(this).fadeOut('fast');
-    })
-})
+$(document).ready(function() {
+    $('.wtf').click(function(){
+        $(this).fadeOut('3');
+    });
+});
+
+$(document).ready(function(){
+    $(".pull-me").click(function() {
+    $(".panel").slideToggle("slow");
+    });
+});
