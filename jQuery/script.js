1894204b1629d40e2bb1027e44eeee692313acef
Fibiola <natasha.pristovsek@gmail.com>
2013-09-15 20:00:33 +0200

    added moving Mario

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,7 +1,7 @@
 //this only affects the div we want it to affect - instead of all divs simultaniously.
 $(document).ready(function() {
     $('.buttons').mouseenter(function() {
-        $(this).fadeTo('fast', 0);
+        $(this).toggleClass('cool');
     });
 });
 
@@ -12,8 +12,61 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
+    $('.cool').mouseenter(function(){
+        $(this).toggleClass("wtf");
+    });
+});
+
+$(document).ready(function() {
     $(".pull-me").click(function() {
     $(".panel").slideToggle("slow");
+    });
+});
+
+// To -do list
+$(document).ready(myClick);
+
+function myAdd(){
+    var toAdd = $('input[name=checkListItem]').val();
+    $('.list').append('<div class="item">' + toAdd + '</div>');
+}
+
+function myClick(){
+    $('#button').click(myAdd);
+    $(document).on('click', '.item', myRemove);
+}
+
+function myRemove(){
+    $(this).remove();
+}
+//aletrnative to do list function
+$(document).ready(function() {
+    $('button').click(function() {
+        var toAdd = $("input[name=message]").val();
+        $('#messages').append("<p>"+toAdd+"</p>");
+    });
+});
+
+//super mario code - to move left right up and down.
+
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+            case 65:
+                $('img').animate({left: "-=10px"}, 'fast');
+                break;
+            case 83:
+                $('img').animate({top: "+=10px"}, 'fast');
+                break;
+            case 87:
+                $('img').animate({top: "-=10px"}, 'fast');
+                break;
+            case 68:
+                $('img').animate({left: "+=10px"}, 'fast');
+                break;
+            default:
+                break;
+        }
     });
 });
